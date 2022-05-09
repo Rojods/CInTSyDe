b8f7917d1f8bc606db54652c4173a5bc33ff04a3
Rodolfo Jordoa <jordao@kth.se>
2022-05-09 14:44:20 +0200

    more

--- common/src/main/java/cintsyde/interfaces/DirectoryComponent.java
+++ common/src/main/java/cintsyde/interfaces/DirectoryComponent.java
@@ -1,2 +1,21 @@
-package cintsyde.interfaces;public interface DirectoryComponent {
+package cintsyde.interfaces;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public interface DirectoryComponent<BaseT, ContexT> extends Component<BaseT, ContexT> {
+
+    Path getComponentPath();
+
+    void setComponentPath(Path path);
+
+    default void generate(Path targetPath) throws IOException {
+
+    }
+
+    static boolean isDirectoryComponent(Path path) throws IOException {
+        return Files.isDirectory(path) && Files.list(path).anyMatch(p -> p.getFileName().toString().equals("cintsyde.groovy"));
+    }
 }
--- lib/src/main/xtend-gen/template/baremetal/ExternalDataBlockInc.java
+++ lib/src/main/xtend-gen/template/baremetal/ExternalDataBlockInc.java
@@ -2,44 +2,20 @@ package template.baremetal;
 
 import fileAnnotation.FileType;
 import fileAnnotation.FileTypeAnno;
-import forsyde.io.java.core.ForSyDeSystemGraph;
-import forsyde.io.java.core.Vertex;
-import generator.Generator;
-import java.util.Set;
-import org.eclipse.xtend2.lib.StringConcatenation;
 import template.templateInterface.InitTemplate;
-import utils.Query;
 
 @FileTypeAnno(type = FileType.C_INCLUDE)
 @SuppressWarnings("all")
 public class ExternalDataBlockInc implements InitTemplate {
   public String create() {
-    String _xblockexpression = null;
-    {
-      ForSyDeSystemGraph model = Generator.model;
-      Set<Vertex> externDataBlocks = Query.findAllExternalDataBlocks(model);
-      StringConcatenation _builder = new StringConcatenation();
-      _builder.append("#ifndef EXTERNAL_DATABLOCK_H_");
-      _builder.newLine();
-      _builder.append("#define EXTERNAL_DATABLOCK_H_");
-      _builder.newLine();
-      _builder.newLine();
-      {
-        for(final Vertex data : externDataBlocks) {
-          _builder.append("#define ");
-          String _upperCase = data.getIdentifier().toUpperCase();
-          _builder.append(_upperCase);
-          _builder.append("_BLOCKING 0");
-          _builder.newLineIfNotEmpty();
-        }
-      }
-      _builder.newLine();
-      _builder.append("#endif ");
-      _builder.newLine();
-      _builder.newLine();
-      _xblockexpression = _builder.toString();
-    }
-    return _xblockexpression;
+    throw new Error("Unresolved compilation problems:"
+      + "\nno viable alternative at input \'Â\'"
+      + "\nno viable alternative at input \'»\\t\\t\\r\\n\\r\\n\\t\\t#endif \\r\\n\\t\\t\\r\\n\\t\\t\'\'\'\'"
+      + "\nThe method or field externDataBlocksÂ is undefined"
+      + "\nThe method or field Â is undefined"
+      + "\nThe method or field ENDFORÂ is undefined"
+      + "\ngetIdentifier cannot be resolved"
+      + "\ntoUpperCase cannot be resolved");
   }
   
   public String getFileName() {
